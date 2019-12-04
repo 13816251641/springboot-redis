2fde70b8731b747147841e7ebaa828a856493fa4
admin <982584989@qq.com>
2019-12-04 21:56:52 +0800

    自己实现事务

--- src/main/java/com/lujieni/service/TransactionServiceWithMyOwn.java
+++ src/main/java/com/lujieni/service/TransactionServiceWithMyOwn.java
@@ -21,8 +21,15 @@ public class TransactionServiceWithMyOwn {
 
     /*
         当enableTransactionSupport为false的时候的确会close连接
+        日志:
         Opening RedisConnection
         Closing bound connection.
+
+        redisTemplate.execute方法中会得到连接并将连接绑定到当前线程中,
+        之后operations.opsForSet().add代码的连接都是从当前线程中获取的,
+        所以事务成立。最后再execute方法中会调用RedisConnectionUtils.unbindConnection(factory);
+        关闭连接,关闭连接代表将连接归还给连接池,不意味着切断和redis的连接!!!
+
      */
     public void test1(){
         List<Object> txResults = redisTemplate.execute(new SessionCallback<List<Object>>() {
